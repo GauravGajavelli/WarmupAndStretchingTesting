7c6777ebb4d2da0e5efc5423ebb52626302a1c09
gajavegs <gauravsg2004@gmail.com>
2025-07-12 23:59:55 -0500

    Start repo

---
+++ src/list/SortedLinkedList.java
@@ -0,0 +1,55 @@
+package list;
+
+/**
+ * 
+ * @author anderson, chenett1
+ * 
+ * @param <T>
+ *            Any Comparable type
+ * 
+ *            Stores a doubly linked list whose elements are kept in sorted order.
+ */
+public class SortedLinkedList<T extends Comparable<T>> {
+	
+	DoublyLinkedList<T> dll;
+
+	/**
+	 * Creates an empty list
+	 * 
+	 */
+	public SortedLinkedList() {
+		dll = new DoublyLinkedList<T>();
+	}
+
+	/**
+	 * Creates a sorted list containing the same elements as the parameter
+	 * 
+	 * @param other
+	 *            the input list
+	 */
+	public SortedLinkedList(DoublyLinkedList<T> other) {
+		this();
+		DLLIterator<T> i = other.iterator(); // Creates an iterator using the factory method
+		while (i.hasNext()) {     // Note how the iterator is used
+			this.add(i.next());   // This line uses your add method below
+		}	
+	}
+
+	/**
+	 * Adds the given element to the list, keeping it sorted.
+	 */
+	public void add(T element) {
+		// TODO: implement this method
+		// Hint: the Node class is package-accessible via DoublyLinkedList<T>.Node
+                throw new UnsupportedOperationException("TODO: delete this statement and implement this operation.");
+	}
+	
+	/**
+	 * Prints the internal list.
+	 */
+	@Override
+	public String toString() {
+		return dll.toString();
+	}
+
+}
---
+++ src/priorityQueue/ArrayListMinPQ.java
@@ -0,0 +1,60 @@
+package priorityQueue;
+import java.util.ArrayList;
+
+/**
+ * An implementation of the Priority Queue interface using an array list.
+ * 
+ * @author Matt Boutell and <<TODO: Your name here>>>. Created Mar 29, 2014.
+ * 
+ * @param <T>
+ *            Generic type of PQ elements
+ */
+public class ArrayListMinPQ<T extends Comparable<T>> {
+	// Requirement: The methods below must use the items list.
+	// You may not add other fields to this ArrayListMinPQ class, 
+	// but you may add local variables to the methods. 
+	private ArrayList<T> items;
+
+	public ArrayListMinPQ() {
+		// TODO: implement
+		throw new UnsupportedOperationException("TODO: delete this statement and implement this operation.");
+
+	}
+
+	public T findMin() {
+		// This is also known as peekMin
+		// TODO: implement
+		throw new UnsupportedOperationException("TODO: delete this statement and implement this operation.");
+
+	}
+
+	public T deleteMin() {
+		// TODO: implement
+		throw new UnsupportedOperationException("TODO: delete this statement and implement this operation.");
+
+	}
+
+	public void insert(T item) {
+		// TODO: implement
+		throw new UnsupportedOperationException("TODO: delete this statement and implement this operation.");
+
+	}
+
+	public int size() {
+		// TODO: implement
+		throw new UnsupportedOperationException("TODO: delete this statement and implement this operation.");
+
+	}
+
+	public boolean isEmpty() {
+		// TODO: implement
+		throw new UnsupportedOperationException("TODO: delete this statement and implement this operation.");
+
+	}
+
+	public void clear() {
+		// TODO: implement
+		throw new UnsupportedOperationException("TODO: delete this statement and implement this operation.");
+
+	}
+}
